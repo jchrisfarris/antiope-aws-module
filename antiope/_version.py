574fe681092b021daa3aab234cc59afc81fed696
Chris Farris <chris_farris@discovery.com>
2021-07-09 18:59:07 -0400

    Bump package version

--- antiope/_version.py
+++ antiope/_version.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version_info__ = (1, 2, 0)
+__version_info__ = (1, 2, 1)
 __version__ = '.'.join(str(d) for d in __version_info__)
 
 if __name__ == '__main__':
